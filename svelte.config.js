4cb9046a6b2cc43bc9993bf3365d81df68d5878a
Brian Keyes <b.keyes@blueglassmoon.com>
2022-10-06 15:01:50 -0500

    Update packages

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,4 @@
+import adapter from '@sveltejs/adapter-auto'
 import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -5,6 +6,10 @@ const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
   preprocess: preprocess(),
+
+  kit: {
+    adapter: adapter(),
+  },
 }
 
 export default config
