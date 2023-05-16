de484cdeb2f031099a35ad19765992f4a65f5bd6
Brian Keyes <b.keyes@blueglassmoon.com>
2023-05-15 19:03:56 -0500

    Delete previous code version

--- src/lib/index.ts
+++
@@ -1,3 +0,0 @@
-export { clear } from './_functions/clear.js'
-export { update } from './_functions/update.js'
-export { swr } from './_functions/swr.js'
--- src/lib/plugin.ts
+++
@@ -1,4 +0,0 @@
-export { refreshInterval } from './_plugins/refresh-interval.js'
-export { refreshOnFocus } from './_plugins/refresh-on-focus.js'
-export { refreshOnReconnect } from './_plugins/refresh-on-reconnect.js'
-export { suspend } from './_plugins/suspend.js'
