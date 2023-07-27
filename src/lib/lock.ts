3b374003d44d02f861051494b1cd267b7f3f9d0a
Brian K <b.keyes@blueglassmoon.com>
2023-07-27 14:43:22 -0500

    Reject promise on error

--- src/lib/lock.ts
+++ src/lib/lock.ts
@@ -4,23 +4,20 @@ import type { ModelName } from '$lib/types.js'
 export type LockFn = <T>(
   key: string,
   exclusive: boolean,
-  fn: () => Promise<T>
+  fn: () => Promise<T>,
 ) => Promise<T>
 
 export function Lock(model_name: ModelName): LockFn {
   return function acquireLock<T>(
     key: string,
     exclusive: boolean,
-    fn: () => Promise<T>
+    fn: () => Promise<T>,
   ) {
-    return new Promise<T>((resolve) => {
+    return new Promise<T>((resolve, reject) => {
       navigator.locks.request(
         `${SWR_VERSION}${SEPARATOR}${model_name}${SEPARATOR}${key}`,
         { mode: exclusive ? 'exclusive' : 'shared' },
-        async () => {
-          const result = await fn()
-          resolve(result)
-        }
+        () => fn().then(resolve, reject),
       )
     })
   }
