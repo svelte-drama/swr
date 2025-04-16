7b981cd389fb4a9632e97307495c85b669d9383b
Brian K <b.keyes@blueglassmoon.com>
2025-04-16 15:16:44 -0500

    Implement lock polyfill for use when running under Node

--- src/lib/lock.ts
+++ src/lib/lock.ts
@@ -7,7 +7,9 @@ export type LockFn = <T>(
   fn: () => Promise<T>,
 ) => Promise<T>
 
-export function Lock(model_name: ModelName): LockFn {
+export const Lock = navigator.locks ? LockPolyfill : NavigatorLock
+
+function NavigatorLock(model_name: ModelName): LockFn {
   return async function acquireLock<T>(
     key: string,
     exclusive: boolean,
@@ -19,5 +21,27 @@ export function Lock(model_name: ModelName): LockFn {
       { mode: exclusive ? 'exclusive' : 'shared' },
       fn,
     )
+  }
+}
+
+const queue = new Map<string, Promise<any>>()
+
+function LockPolyfill(model_name: ModelName): LockFn {
+  return async function acquireLock<T>(
+    key: string,
+    exclusive: boolean,
+    fn: () => Promise<T>,
+  ) {
+    const lock = `${SWR_VERSION}${SEPARATOR}${model_name}${SEPARATOR}${key}`
+
+    const previous = queue.get(lock) ?? Promise.resolve()
+    const next = previous.then(fn, fn).finally(() => {
+      if (queue.get(lock) === next) {
+        queue.delete(lock)
+      }
+    })
+    queue.set(lock, next)
+
+    return next
   }
 }
